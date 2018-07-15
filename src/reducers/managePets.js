a64185a3205c62647d07626522d9898e8150646a
github username <github email address>
2018-07-15 10:19:26 -0400

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,38 @@
 export let state;
 
+export function managePets(state = {pets: []}, action){
 
-export function managePets(){
+    switch (action.type) {
+        case "ADD_PET":
+
+        return {pets: [...state.pets, action.pet]}
+        case "REMOVE_PET":
+
+        return {pets: state.pets.filter(pet => pet.id !== action.id)}
+        default:
+        return state
+    }
 }
 
-export function dispatch(){
+// dispatch should pass an action to the reducer and use that return value to update the state, a globally accessible variable
+export function dispatch(action){
+    state = managePets(state, action)
+    render()
+
 }
 
 export function render(){
+    const contain = document.getElementById("container")
+
+    state.pets.forEach(pet => {
+        contain.innerHTML = `<ul><li>${pet.name}</li></ul>`   
+    });
+
+    
+    
 
 }
+// Since our users want to see their pets on a webpage we want to have a render method that inserts a <ul> to the DOM with each pet's name wrapped in an <li>. The <ul> should be a child of an element with the id of container. There's no need to load jQuery into our app for such a small task. We can make use of built-in JavaScript methods like document.getElementById.
+
+
+
